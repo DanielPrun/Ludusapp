7f44d0e75d15df415c55d59b63768f6248ce0d6b
DanielPrun <114423058+DanielPrun@users.noreply.github.com>
2024-06-25 03:26:58 +0200

    Update model.js
    Fix get models

--- routes/model.js
+++ routes/model.js
@@ -4,7 +4,7 @@ const ModelController = require("../controllers/model");
 const check = require("../middlewares/auth");
 
 
-router.get("/models", check.auth, ModelController.getModels);
+router.get("/models",  ModelController.getModels);
 router.get("/model/:id", check.authEntrenador, ModelController.getModel);
 router.post("/guardar", check.authEntrenador, ModelController.guardar);
 router.put("/editar/:id", check.authEntrenador, ModelController.editar);
